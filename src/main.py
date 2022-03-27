5840e349f386e115e0bf5ae2da850dcac98f881d
stonesha-goarmstrong <36431006+stonesha@users.noreply.github.com>
2022-03-26 17:42:19 -0700

    initial commit
    Contains boilerplate code for FastAPI and fetching FE13:
    - Base Growths
    - Characters
    - Skills

---
+++ src/main.py
@@ -0,0 +1,51 @@
+import routing
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+app = FastAPI()
+
+
+@app.get('/')
+def main():
+    return 'FireEmblemAPI - The Unofficial Fire Emblem API :)'
+
+
+@app.get('/error')
+def error():
+    return 'Nothing to see here :('
+
+
+@app.get('/games')
+async def get_games():
+    return routing.get_games()
+
+
+@app.get('/{game}/character/{character}')
+async def get_character(game: str, character: str):
+    return routing.get_character(game, character)
+
+
+@app.get('/{game}/characters')
+async def get_characters(game: str):
+    return routing.get_characters(game)
+
+
+@app.get('/{game}/skills')
+async def get_skills(game: str):
+    return routing.get_skills(game)
+
+
+@app.get('/{game}/base_growths/{character}')
+async def get_base_growths_for_character(game: str, character: str):
+    return routing.get_base_growths_for_character(game, character)
+
+
+@app.get('/{game}/base_growths')
+async def get_base_growths(game: str):
+    return routing.get_base_growths(game)
+
+
+@app.exception_handler(StarletteHTTPException)
+def custom_http_exception_handler(request, exc):
+    return RedirectResponse('/error')
---
+++ src/models.py
@@ -0,0 +1,44 @@
+import json
+
+
+class Game():
+
+    def __init__(self, title: str):
+        self.title = title
+
+    def __load_json(self, json_file: str) -> list[dict]:
+        with open(json_file, 'r') as f:
+            return json.load(f)
+
+    def get_character(self, name: str) -> dict or None:
+        characters = self.get_characters()
+
+        for character in characters:
+            if character['name'].replace('\'', '').lower() == name.lower():
+                return character
+        else:
+            return None
+
+    def get_characters(self) -> list[dict]:
+        characters = self.__load_json('%s/characters.json' % self.title)
+
+        return characters
+
+    def get_skills(self) -> list[dict]:
+        skills = self.__load_json('%s/skills.json' % self.title)
+
+        return skills
+
+    def get_base_growths_for_character(self, character: str) -> dict or None:
+        base_growths = self.get_base_growths()
+
+        for base_growth in base_growths:
+            if base_growth['name'].replace('\'', '').lower() == character.lower():
+                return base_growth
+        else:
+            return None
+
+    def get_base_growths(self) -> list[dict]:
+        base_growths = self.__load_json('%s/base_growths.json' % self.title)
+
+        return base_growths
